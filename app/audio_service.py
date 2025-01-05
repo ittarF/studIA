9ba423ef7aac448b500c3dd20aec59979e609eb1
ittarF <ittarf@gmail.com>
2025-01-06 00:41:57 +0100

    tried to use gpu but didnt have sample...

--- app/audio_service.py
+++ app/audio_service.py
@@ -15,3 +15,19 @@ print('Loading model')
 processor = Wav2Vec2Processor.from_pretrained(MODEL_ID)
 model = Wav2Vec2ForCTC.from_pretrained(MODEL_ID)
 
+import time
+input_values = processor(speech_array, return_tensors="pt", sampling_rate=16000).input_values
+
+# calculate time
+start_time = time.time()
+with torch.no_grad():
+    logits = model(input_values).logits
+
+predicted_ids = torch.argmax(logits, dim=-1)
+predicted_sentences = processor.batch_decode(predicted_ids)
+end_time = time.time()
+for i, predicted_sentence in enumerate(predicted_sentences):
+    print("-" * 100)
+    print("Prediction:", predicted_sentence)
+    print("-" * 100)
+    print(f"Time elapsed: {end_time - start_time:.2f} seconds for a {speech_array.shape[0] / sampling_rate:.2f} seconds audio")
